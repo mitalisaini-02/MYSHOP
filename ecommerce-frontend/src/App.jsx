48106a59677e79757fdae9561b45b0c1a402fb70
mitalisaini-02 <mitalisaini9480@gmail.com>
2025-06-24 03:22:11 +0530

    Fixed missing ScrollToTop import issue

--- ecommerce-frontend/src/App.jsx
+++ ecommerce-frontend/src/App.jsx
@@ -2,7 +2,7 @@ import Home from './pages/Home';
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import './styles/variables.css';
-import ScrollToTop from './pages/ScrollToTop'; // adjust the path as needed
+import ScrollToTop from './pages/scrollToTop'; // adjust the path as needed
 
 import Navbar from './components/Navbar';
 import Cart from './pages/Cart'; // Make sure the path and casing match your file exactly
